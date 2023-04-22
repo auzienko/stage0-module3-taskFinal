e2b52c33fa7537590246a591b0397fa21618b7a0
Aleksandr Uzienko <a.uzienko@gmail.com>
2023-04-22 09:51:25 +0300

    solution

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int first = number % 10;
+        int second = number /10 % 10;
+        int third = number /100 % 10;
+        int fourth = number /1000 % 10;
+
+        System.out.println(first + second + third + fourth);
     }
 }
